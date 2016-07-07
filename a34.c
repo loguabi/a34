0ca55232f8656d46c1466fe198b0c63fa269e036
logitha <loga848.456@gmail.com>
2016-07-07 19:24:55 +0530

    Create a34.c

---
+++ a34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a[30],d;
+printf("Enter the string:");
+scanf("%s",&a);
+if(isdigit(a));
+{
+printf("false\n");
+}
+
+else
+{
+printf("true\n");
+}
+return 0;
+}
